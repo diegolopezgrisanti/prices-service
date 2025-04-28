4c3d09a031a2e3c53b4ce07d96ff89b84b205335
diegolopezgrisanti <diegolopezgrisanti@gmail.com>
2025-04-28 17:42:52 -0300

    first commit

---
+++ src/main/java/com/inditex/prices/service/PricesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.prices.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inditex/prices/service/PricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.prices.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
